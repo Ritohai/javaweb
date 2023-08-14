b6a077d68b1f0100cc5e246abe22e5f4b4cc3330
Ritohai <128031880+Ritohai@users.noreply.github.com>
2023-08-14 09:15:04 +0700

    Add files via upload

---
+++ Session17-TH1/src/main/java/org/example/controller/Greeting.java
@@ -0,0 +1,22 @@
+package org.example.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class Greeting {
+    @GetMapping("/greeting")
+    public String greeting(@RequestParam String name, Model model) {
+        model.addAttribute("name", name);
+        return "hello";
+    }
+
+    @RequestMapping(value = "/")
+    public String home(){
+        return "home";
+    }
+}
+
