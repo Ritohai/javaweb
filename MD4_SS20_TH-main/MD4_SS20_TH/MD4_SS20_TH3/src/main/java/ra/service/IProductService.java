085440fc292dd1444229e75575a50a94f12283ed
Ritohai <128031880+Ritohai@users.noreply.github.com>
2023-08-18 08:09:57 +0700

    Add files via upload

---
+++ MD4_SS20_TH-main/MD4_SS20_TH/MD4_SS20_TH3/src/main/java/ra/service/IProductService.java
@@ -0,0 +1,19 @@
+package ra.service;
+
+import ra.model.Product;
+
+import java.util.List;
+
+public interface IProductService {
+    List<Product> findAll();
+
+    void save(Product product);
+
+    Product findById(int id);
+
+    void update(int id, Product product);
+
+    void remove(int id);
+
+}
+
